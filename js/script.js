f7101f95ba039e95efb89b99d0867c6c4bb40ce1
Noor Timraz <noortimraz2001@gmail.com>
2025-06-09 19:25:49 +0300

    صفحة تسجيل دخول المعلم والمديير

--- js/script.js
+++ js/script.js
@@ -1,19 +1,62 @@
 
-  function showForm(type, clickedIcon) {
-    const studentForm = document.getElementById("stdForm");
-    const parentForm = document.getElementById("parentForm");
-    const iconStd = document.getElementById("icon-std").querySelector("img");
-    const iconParent = document.getElementById("icon-parent").querySelector("img");
-
-    if (type === "student") {
-      studentForm.style.display = "block";
-      parentForm.style.display = "none";
-      iconStd.src = "images/std-on.png";
-      iconParent.src = "images/par-off.png";
-    } else if (type === "parent") {
-      studentForm.style.display = "none";
-      parentForm.style.display = "block";
-      iconStd.src = "images/std-off.png";
-      iconParent.src = "images/par-on.png";
-    }
+// الانتقال بين النماذج فيي صفحات اللوقن
+function showForm(type, clickedIcon) {
+    const formGroups = {
+      student: {
+        show: "stdForm",
+        hide: "parentForm",
+        activeImg: "images/std-on.png",
+        inactiveImg: "images/par-off.png",
+        activeIcon: "icon-std",
+        inactiveIcon: "icon-parent"
+      },
+      parent: {
+        show: "parentForm",
+        hide: "stdForm",
+        activeImg: "images/par-on.png",
+        inactiveImg: "images/std-off.png",
+        activeIcon: "icon-parent",
+        inactiveIcon: "icon-std"
+      },
+      teacher: {
+        show: "teacherForm",
+        hide: "adminForm",
+        activeImg: "images/teacher.png",
+        inactiveImg: "images/manager-off.png",
+        activeIcon: "icon-teacher",
+        inactiveIcon: "icon-admin"
+      },
+      admin: {
+        show: "adminForm",
+        hide: "teacherForm",
+        activeImg: "images/manager.png",
+        inactiveImg: "images/teacher-off.png",
+        activeIcon: "icon-admin",
+        inactiveIcon: "icon-teacher"
+      }
+    };
+  
+    // التأكد إن النوع صحيح
+    if (!formGroups[type]) return;
+  
+    const group = formGroups[type];
+    const showFormEl = document.getElementById(group.show);
+    const hideFormEl = document.getElementById(group.hide);
+    const activeIconImg = document.getElementById(group.activeIcon).querySelector("img");
+    const inactiveIconImg = document.getElementById(group.inactiveIcon).querySelector("img");
+  
+    hideFormEl.style.display = "none";
+    showFormEl.style.display = "block";
+    showFormEl.classList.add("fade-in");
+  
+    activeIconImg.src = group.activeImg;
+    inactiveIconImg.src = group.inactiveImg;
+  
+    document.querySelectorAll('.icon, .login-icon').forEach(icon => {
+      icon.classList.remove('active-icon');
+    });
+    clickedIcon.classList.add('active-icon');
   }
+  
+
+
